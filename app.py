324cf21ebb9603883d10d3075484780e48761e3e
anyel-ec <paulangel826@gmail.com>
2024-05-18 07:45:05 -0500

    detect port scan of all, not host, in developemnt

---
+++ app.py
@@ -0,0 +1,22 @@
+from scapy.all import sniff
+from scapy.layers.inet import TCP, IP
+ 
+# Dirección IP de tu máquina
+MY_IP = "192.168.0.106"
+ 
+def detect_port_scan(packet):
+    if packet.haslayer(TCP) and packet.haslayer(IP):
+        if packet[IP].dst == MY_IP:  
+            # Verifica si el paquete tiene solo el flag SYN activado
+            if packet[TCP].flags == "S":
+                ip_src = packet[IP].src
+                tcp_dport = packet[TCP].dport
+                print(f"Posible escaneo de puertos detectado desde IP: {ip_src}, en el puerto: {tcp_dport}")
+
+def main():
+    print("Iniciando la detección de escaneo de puertos...")
+    # Captura paquetes TCP entrantes hacia tu IP
+    sniff(filter=f"tcp and dst host {MY_IP}", prn=detect_port_scan, store=0)
+
+if __name__ == "__main__":
+    main() 
