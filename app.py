cbb60e0adb29cf2aaa5188e2a49256dc7546625c
anyel-ec <paulangel826@gmail.com>
2024-06-22 22:35:43 -0500

    add msj of whatsapp

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from scapy.all import sniff
 from scapy.layers.inet import TCP, IP
  
 # Dirección IP de tu máquina
-MY_IP = "192.168.0.106"
+MY_IP = "192.168.1.7"
  
 def detect_port_scan(packet):
     if packet.haslayer(TCP) and packet.haslayer(IP):
@@ -12,6 +12,9 @@ def detect_port_scan(packet):
                 ip_src = packet[IP].src
                 tcp_dport = packet[TCP].dport
                 print(f"Posible escaneo de puertos detectado desde IP: {ip_src}, en el puerto: {tcp_dport}")
+                # Envien msj 
+                # enviar emails
+                
 
 def main():
     print("Iniciando la detección de escaneo de puertos...")
